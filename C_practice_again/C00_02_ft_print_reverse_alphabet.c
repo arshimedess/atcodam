c6daabff91ff559d6e6f83a40fc744c71a9261e5
Arash Hosseini Koupaei <ahossein@f0r5s11.codam.nl>
2026-08-11 23:33:12 +0200

    keep moving

--- C_practice_again/C00_02_ft_print_reverse_alphabet.c
+++ C_practice_again/C00_02_ft_print_reverse_alphabet.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   C00_01_ft_print_alphabet.c                         :+:    :+:            */
+/*   C00_02_ft_print_reverse_alphabet.c                 :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ahossein <ahossein@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2026/08/11 23:19:34 by ahossein      #+#    #+#                 */
-/*   Updated: 2026/08/11 23:28:37 by ahossein      ########   odam.nl         */
+/*   Created: 2026/08/11 23:29:40 by ahossein      #+#    #+#                 */
+/*   Updated: 2026/08/11 23:31:54 by ahossein      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-void ft_print_alphabet(void)
+
+void ft_print_reverse_alphabet(void)
 {
-	char c = 'a';
-	// while(c >= 97 && c <= 122)
-	while(c <= 122)
+	char c = 'z';
+	while(c >= 'a')
 	{
 		write(1, &c, 1);
-		c++;
+		c--;
 	}
 }
 
 int main(void)
 {
-	ft_print_alphabet();
+	ft_print_reverse_alphabet();
 	return (0);
 }
