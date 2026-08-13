10f8917aa7974b46b395cb557fdcff8280793fdd
Arash Hosseini Koupaei <ahossein@f0r4s20.codam.nl>
2026-08-14 00:32:24 +0200

    nice

--- C_practice_again/_strcpy.c
+++ C_practice_again/_strcpy.c
@@ -9,7 +9,29 @@
 
 // char    *ft_strcpy(char *s1, char *s2);
 
-char    *ft_strcpy(char *s1, char *s2)
+// #include <string.h>
+// #include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src)
 {
-	
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+
+// int	main(void)
+// {
+// 	char	s1[10];"whatever";
+// 	char	s2[20]="";
+
+// 	ft_strcpy(s2, s1);
+// 	printf("s2 now is: %s\n", s2);
+// 	return (0);
+// }
