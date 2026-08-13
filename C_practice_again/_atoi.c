81c04b10379f0034f26585c66e79166a5eb12a2a
Arash Hosseini Koupaei <ahossein@f0r4s20.codam.nl>
2026-08-14 00:10:31 +0200

    atoi added

--- C_practice_again/_atoi.c
+++ C_practice_again/_atoi.c
@@ -11,3 +11,34 @@
 // Your function must be declared as follows:
 
 // int	ft_atoi(const char *str);
+
+#include <stdio.h>
+
+int ft_atoi(const char *str)
+{
+	int i = 0;
+	int nbr = 0;
+	int sign = 1;
+
+	if (str[0] == '-' || str[0] == '+')
+	{
+		if (str[0] == '-')
+		{
+			sign = sign * -1; 
+		}
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		nbr = (nbr * 10) + str[i] - '0';
+		i++;
+	}
+	return (sign * nbr);
+
+
+}
+int main(void)
+{
+	printf("%d", ft_atoi("2345"));
+	return(0);
+}
